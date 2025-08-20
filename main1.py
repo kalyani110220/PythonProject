e737b96ea1f8e5955498b03f55ab27b406ebc8d1
Kalyani <Kalyani.nelluri54@gmil.com>
2025-08-20 12:59:54 +0100

    excerise 2

---
+++ main1.py
@@ -0,0 +1,7 @@
+# Exercise1 Rectangle Area Clac
+
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = length * width
+print(f"The area of {length} x {width} is {area}")
