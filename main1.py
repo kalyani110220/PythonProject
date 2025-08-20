24eda365c7c09bd5cf9ecfd76fa79acafe29391c
Kalyani <Kalyani.nelluri54@gmil.com>
2025-08-20 13:19:42 +0100

    excerise 2

--- main1.py
+++ main1.py
@@ -4,4 +4,12 @@
 length = float(input("Enter the length: "))
 width = float(input("Enter the width: "))
 area = length * width
-print(f"The area of {length} x {width} is {area}")
+print(f"The area of {length} x {width} is {area}cm2")
+
+#  Exercise 2 Shopping Cart Program
+item = input("What item you would like to buy?: ")
+price = float(input("Enter the price: "))
+quantity= int(input("Enter the quantity: "))
+total = price * quantity
+print(f"You have bought {quantity} of {item} at {total}.")
+print(f"The total price is {total}")
