27293cff62ea9c4ec03671afb297406f190d0d36
Kalyani <Kalyani.nelluri54@gmil.com>
2025-08-24 15:48:43 +0100

    Slicing

--- slicing string.py
+++ slicing string.py
@@ -6,5 +6,5 @@ from main import first_name
 name = "Kalyani Nelluri"
 first_name  = name[:7]   #[0:7]
 last_name = name[8:]      #[8:end]
-full_name = name[ : : 11]   #[0:end:6]
-print(full_name)
+funky_name = name[ : : 11]   #[0:end:6]
+print(funky_name)
