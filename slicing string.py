95681dd66f159a13f8a74441da7e703db3cb565a
Kalyani <Kalyani.nelluri54@gmil.com>
2025-08-26 14:33:42 +0100

    #logical operator =  evaluate multiple conditions (or,and,not)

--- slicing string.py
+++ slicing string.py
@@ -8,3 +8,15 @@ first_name  = name[:7]   #[0:7]
 last_name = name[8:]      #[8:end]
 funky_name = name[ : : 11]   #[0:end:6]
 print(funky_name)
+
+#logical operator =  evaluate multiple conditions (or,and,not)
+#                      or = at least one condition must be True
+#                       and = both conditions must be True
+#                        not = inverts the condition (not False, not True)
+
+temp = 25
+is_sunny = True
+
+if temp >= 28 and  is_sunny:
+    print("It is Hot outside🥵 ")
+    print(" IT is sunny ☀️")
