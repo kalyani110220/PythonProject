37b05a3cadcd902126ef35f074315dc774560cf3
Kalyani <Kalyani.nelluri54@gmil.com>
2025-08-28 18:33:30 +0100

    # for loops = execute a block of code a fixed number of times. #              you can iterate over a range,string,sequence,etc

---
+++ for loop.py
@@ -0,0 +1,8 @@
+# for loops = execute a block of code a fixed number of times.
+#              you can iterate over a range,string,sequence,etc
+
+for i in range(1,21):
+    if i== 13:
+        break
+    else:
+        print(i)
